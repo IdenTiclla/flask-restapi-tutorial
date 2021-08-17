756ee759323cc384af0f9424501a78419461f7fc
unknown <iden.ticlla@gmail.com>
2021-08-17 08:55:27 -0400

    passing parameters

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
+names = {"tim": {"age": 12, "gender": "male"}, "bill": {"age": 12, "gender": "male"}}
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello world"}
+    def get(self, name):
+        return names[name]
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
